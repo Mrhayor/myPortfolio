63b6e06f5a7dcf31ffd520de616e3a92a506626b
Mrhayor <soluwaseun946@gmail.com>
2024-01-25 15:16:34 +0100

    first commit

---
+++ src/Components/Portfolio.jsx
@@ -0,0 +1,26 @@
+export default function Portfolio() {
+  return (
+    <div>
+        <header>My Portfolio</header>
+        <div>
+            <ul>
+                <li>
+                    Name: Oluwaseun Samuel Odunayo
+                    linkedin:
+                    githhub:
+                    
+                </li>
+            </ul>
+
+            <div>
+                <h1>Projects worked on</h1>
+                <div>
+                    <span><img src="./src/hrlibrary.png" alt="scsd" /></span>
+                    <a href="https://mrhayor.github.io/HrLibrary">mrhayor.github.io/HrLibrary</a>
+                    hrLibrary platform that is used to search the 
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
